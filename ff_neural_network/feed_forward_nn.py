0b39c5c9927ccf17ac58b322d1a7e548188410a0
Shah Zafrani <shahzafrani@gmail.com>
2018-03-26 19:27:36 -0400

    now have a proper dummy feedforward network

--- ff_neural_network/feed_forward_nn.py
+++ ff_neural_network/feed_forward_nn.py
@@ -1,31 +1,36 @@
+# feed_forward_nn.py
+# python 2.7.14
+# 3 layer feed-forward neural network
+# MNIST classier
+# Shah Zafrani
+
+from sklearn.model_selection import KFold
 import numpy as np
-# class feed_forward_neural_network (object):
-#
-#     # create constructor
-#     def __init__(self, input, hidden, output):
-#         self.input = input
-#         self.hidden = hidden
-#         self.output = output
 
 def normalize(x):
     return x / 255.0
 
-def sigmoid(z):
-    return (1.0/(1.0 + np.exp(-z)))
+def sigmoid(z, derivative=False):
+    if derivative is False:
+        return (1.0/(1.0 + np.exp(-z)))
+    else:
+        # taken from https://github.com/MichalDanielDobrzanski/DeepLearningPython35/blob/master/network2.py
+        return sigmoid(z)*(1-sigmoid(z))
 
-def softmax(x):
-  e_x = np.exp(x - np.max(x))
-  return e_x / e_x.sum()
+def softmax(x, derivative=False):
+    if derivative is False:
+        e_x = np.exp(x - np.max(x))
+        return e_x / e_x.sum()
+    else:
+        return x
 
 
-num_steps = 1000
+num_steps = 1 #1000
 learning_rate = 1e-2
+num_folds = 2 #10
 input_size = 784
 hidden_size = 30
 output_size = 10
-input_layer = np.ones([input_size])
-hidden_layer = np.ones([hidden_size])
-output_layer = np.ones([output_size])
 
 input_to_hidden_synapses = np.random.randn(input_size, hidden_size)
 hidden_to_output_synapses = np.random.randn(hidden_size, output_size)
@@ -36,18 +41,28 @@ mnist_data = np.genfromtxt('MNIST.csv', delimiter=',', dtype=int, skip_header=1)
 
 labels = mnist_data[:, 0]
 mnist_data = normalize(mnist_data[:, 1:])
-l1 = np.zeros(30)
-for i in range(input_to_hidden_synapses.shape[1]):
-    l1[i] = sigmoid(np.dot(input_layer, input_to_hidden_synapses.T[i]))
-# print(l1)
-l2 = np.zeros(10)
-for i in range(hidden_to_output_synapses.shape[1]):
-    l2[i] = sigmoid(np.dot(hidden_layer, hidden_to_output_synapses.T[i]))
-output = softmax(l2)
 
-prediction = list(output).index(max(output))
+kf = KFold(n_splits=num_folds)
+kf.get_n_splits(mnist_data)
 
-print(prediction)
+for train_index, test_index in kf.split(mnist_data):
+    # print(train_index)
+    for step in range(num_steps):
+        for m in train_index:
+            l1 = np.zeros(hidden_size)
+            # assert(len(mnist_data[m]) == input_to_hidden_synapses.shape[0])
+            for i in range(input_to_hidden_synapses.shape[1]):
+                l1[i] = sigmoid(np.dot(mnist_data[m], input_to_hidden_synapses.T[i]))
+            # print(l1)
+            l2 = np.zeros(output_size)
+            # assert(input_to_hidden_synapses.shape[1] == hidden_to_output_synapses.shape[0])
+            for i in range(hidden_to_output_synapses.shape[1]):
+                l2[i] = np.dot(l1, hidden_to_output_synapses.T[i])
+            output = softmax(l2)
+
+            prediction = list(output).index(max(output))
+            if m % 100 == 0:
+                print("prediction {} : actual {}".format(prediction, labels[m]))
 
 
 # print(labels)
